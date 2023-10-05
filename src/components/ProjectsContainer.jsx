9c1e95802600195ed40d8d5ebd8aad8b19e17354
guilherme-antunes <112368848+guilherme-antunes@users.noreply.github.com>
2023-10-05 15:35:57 -0300

    Add files via upload

---
+++ src/components/ProjectsContainer.jsx
@@ -0,0 +1,16 @@
+
+
+const ProjectsContainer = () => {
+  return (
+   <section className="projects-container"> 
+    <h2>Projetos</h2>
+    <p>Aqui abaixo estão alguns projetos meus no meu repositório do github.</p>
+    <a href="https://github.com/guilherme-antunes?tab=repositories" className="btn">
+      Ver Projetos
+    </a>
+
+   </section>
+  )
+}
+
+export default ProjectsContainer
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import Avatar from "../img/eu.jpg"
+import "../styles/components/sidebar.sass"
+
+import InformationContainer from "./InformationContainer"
+import SocialNetworks from "./SocialNetworks"
+
+const Sidebar = () => {
+
+  return (
+    <aside id="sidebar">
+      <img src={Avatar} alt="Guilherme Antunes" />
+      <p className="title">Desenvolvedor</p>
+      <SocialNetworks />
+      <InformationContainer />
+      <a href="https://drive.google.com/file/d/16eFxf6Z-M-Fz3joKUBeNPdXtzELKAY51/view?usp=drive_link" className="btn">
+        Download Currículo
+      </a>
+    </aside>
+  )
+}
+
+export default Sidebar

5e5bcf050f87930bfc4a76aaeffa2e3a3983ddce
guilherme-antunes <112368848+guilherme-antunes@users.noreply.github.com>
2023-10-17 14:55:10 -0300

    ajustei no about de novo

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -4,7 +4,7 @@ const AboutContainer = () => {
 
   return (
     <section className="about-container">
-        <h2>Sobre mim</h2>
+        <h2>Sobre</h2>
         <p>
            Bacharelado em Ciência da Computação pela Faculdade Meridional IMED de Passo Fundo - RS,
            venho aprimorando minhas skills em programação, desenvolvimento Front-End Web e Mobile.
